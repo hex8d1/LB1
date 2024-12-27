0a538062daa764fbd388ee6931e8a746c734646c
hex8d1 <stavki179@gmail.com>
2024-12-27 02:17:54 +0200

    task4

---
+++ task4.py
@@ -0,0 +1,20 @@
+import asyncio
+from telethon import TelegramClient
+
+api_id = ...
+api_hash = "..."
+phone = "+380XXXXXXXXX"
+
+async def main():
+    client = TelegramClient('my_session', api_id, api_hash)
+    await client.start(phone=phone)
+    participants = await client.get_participants('@...')
+    for user in participants:
+        print(user.id, user.username, user.first_name, user.last_name)
+    await client.send_message('@some_contact', 'Привет!')
+    await client.disconnect()
+
+asyncio.run(main())
+
+
+
